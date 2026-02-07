6f76e3b747667d3ba8d38563fba7033eb2e29609
Milo Sones <102179233+MiloSones@users.noreply.github.com>
2026-02-07 18:06:30 +0000

    fix docker file

--- backend/api/run.py
+++ backend/api/run.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
-from .app import create_app, db
+from app import create_app, db
 
 load_dotenv()
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()  # create tables
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # create tables
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
