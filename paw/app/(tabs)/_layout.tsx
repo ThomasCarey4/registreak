3c8b7d6cc52cbb236129902bc6e494ce4efc8320
Tom Gregg <thomasgregg05@outlook.com>
2026-02-08 02:55:40 +0000

    fix layout page

--- paw/app/(tabs)/_layout.tsx
+++ paw/app/(tabs)/_layout.tsx
@@ -43,12 +43,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="trophy.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          href: null,
-        }}
-      />
     </Tabs>
   );
 }
